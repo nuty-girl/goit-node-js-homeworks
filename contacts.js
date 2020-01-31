7fee4cd3c23b5ec6b29a9c6b4da16e2f6a7dcc3c
unknown <nuty-girl@mail.ru>
2020-01-31 15:43:41 +0200

    hw-2 express, endpoints foby params

--- contacts.js
+++ contacts.js
@@ -6,50 +6,54 @@ const contactsPath = path.resolve(__dirname, "db", "contacts.json");
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
-  console.log("contactsPath:", contactsPath);
-  fs.readFile(contactsPath, (err, data) => {
-    if (err) throw err;
-    const listContacts = JSON.parse(data);
-    console.table(listContacts);
-  });
+  // console.log("contactsPath:", contactsPath);
+  return JSON.parse(fs.readFileSync(contactsPath));
+  // console.table(listContacts);
 }
 
 function getContactById(contactId) {
-  fs.readFile(contactsPath, (err, data) => {
-    if (err) throw err;
-    const listContacts = JSON.parse(data);
-    const contactById = listContacts.find(item => item.id === contactId);
-    console.log("getContactById:", contactById);
-  });
+  const listContacts = JSON.parse(fs.readFileSync(contactsPath));
+  const contactById = listContacts.find(item => String(item.id) === contactId);
+  return contactById;
 }
 
 function removeContact(contactId) {
   fs.readFile(contactsPath, (err, data) => {
     if (err) throw err;
     const listContacts = JSON.parse(data);
-    const filteredContacts = listContacts.filter(item => item.id !== contactId);
-    console.log("filteredContacts:", filteredContacts);
+    return listContacts.filter(item => item.id !== contactId);
   });
 }
 
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath, (err, data) => {
+function addContact({ id, name, email, phone }) {
+  listContacts = JSON.parse(fs.readFileSync(contactsPath));
+  // console.log(typeof listContacts);
+  listContacts.push({
+    id,
+    name,
+    email,
+    phone
+  });
+  // console.log("new list:", listContacts);
+  const jsonListContact = JSON.stringify(listContacts);
+  fs.writeFile(contactsPath, jsonListContact, err => {
     if (err) throw err;
-    listContacts = JSON.parse(data);
-    console.log(typeof listContacts);
-    listContacts.push({
-      id: shortid.generate(),
-      name,
-      email,
-      phone
-    });
-    console.log("new list:", listContacts);
-    const jsonListContact = JSON.stringify(listContacts);
-    fs.writeFile(contactsPath, jsonListContact, err => {
-      if (err) throw err;
-      console.log(`Add new contact ${name}`);
-    });
+    console.log(`Add new contact ${name}`);
   });
 }
 
-module.exports = { listContacts, getContactById, removeContact, addContact };
+function updateContact(contactId, { name, email, phone }) {
+  let contact = getContactById(contactId);
+  if (contact) {
+    contact = { ...contact, ...{ name, email, phone } };
+    return contact;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact
+};
